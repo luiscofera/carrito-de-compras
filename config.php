88db1cfda30a2911bf99815f63150425ecc07f18
swordeman94 <50120386+swordeman94@users.noreply.github.com>
2019-04-29 18:22:56 -0500

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("KEY", "swordeman");
-define("COD", "AES-128-ECB");
-define("SERVIDOR", "localhost:33065");
-define("USUARIO", "root");
-define("PASSWORD", "");
-define("BD", "tienda");
-?>
